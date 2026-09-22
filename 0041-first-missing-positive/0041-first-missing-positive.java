d9ed2819ba06a485354c9450dcabb2788c09f3a7
Ashwin S <ashwinsathishkumar846@gmail.com>
2026-09-22 22:38:26 +0530

    Time: 1 ms (99.94%), Space: 71.2 MB (72.01%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,18 +1,36 @@
+//class Solution {
+   // public int firstMissingPositive(int[] nums) {
+    //    Set<Integer> seen = new HashSet<>();
+   //     for (int num : nums) {
+     //       seen.add(num);
+    //    }
+     //   for (int i = 1; i <= nums.length + 1; i++) {
+    //        if (!seen.contains(i)) {
+    //            return i;
+    //        }
+  //      }
+  //      return nums.length + 1;
+  //  }
+//}
+//change
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        // Store all numbers in a hash set for O(1) lookup
-        Set<Integer> seen = new HashSet<>();
-        for (int num : nums) {
-            seen.add(num);
-        }
-
-        // Check 1, 2, 3, ... until we find one that's missing
-        for (int i = 1; i <= nums.length + 1; i++) {
-            if (!seen.contains(i)) {
-                return i;
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            while (nums[i] >= 1 &&
+                   nums[i] <= n &&
+                   nums[i] != nums[nums[i] - 1]) {
+                int correctIndex = nums[i] - 1;
+                int temp = nums[i];
+                nums[i] = nums[correctIndex];
+                nums[correctIndex] = temp;
             }
         }
-
-        return nums.length + 1;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return n + 1;
     }
 }
